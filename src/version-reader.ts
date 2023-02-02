01c0a4bd2e854b45d501b6ba73d55367c4141dc8
kody <k.kodieaddo@gmail.com>
2023-02-02 13:41:15 +0100

    feat: add method to read all pkgs

--- src/version-reader.ts
+++ src/version-reader.ts
@@ -1,4 +1,5 @@
 import * as fs from 'node:fs/promises'
+import {ERROR, SUCCESS} from './common/types'
 export default class VersionReader {
     private filePath: string;
 
@@ -14,14 +15,73 @@ export default class VersionReader {
 
         if (!prolib && !devLib && !peerLib) {
             return {
-                status: 'ERROR',
+                status: ERROR,
                 body: `Library ${library} not found`,
             }
         }
 
         return {
-            status: 'SUCCESS',
+            status: SUCCESS,
             body: prolib || devLib || peerLib,
+        }
+    }
+
+    async readMany(mode?: string): Promise<{status: 'ERROR' | 'SUCCESS', body: string}> {
+        const packageJson = JSON.parse(await fs.readFile(this.filePath, 'utf8'))
+
+        if (mode === 'prod') {
+            console.log('packageJson.dependencies', packageJson.dependencies)
+            if (!packageJson.dependencies) {
+                console.log('No production dependencies found')
+                return {
+                    status: ERROR,
+                    body: 'No production dependencies found',
+                }
+            }
+
+            return {
+                status: SUCCESS,
+                body: JSON.stringify(packageJson.dependencies),
+            }
+        }
+
+        if (mode === 'dev') {
+            if (!packageJson.devDependencies) {
+                return {
+                    status: ERROR,
+                    body: 'No development dependencies found',
+                }
+            }
+
+            return {
+                status: SUCCESS,
+                body: JSON.stringify(packageJson.devDependencies),
+            }
+        }
+
+        if (mode === 'peer') {
+            if (!packageJson.peerDependencies) {
+                return {
+                    status: ERROR,
+                    body: 'No peer dependencies found',
+                }
+            }
+
+            return {
+                status: SUCCESS,
+                body: JSON.stringify(packageJson.peerDependencies),
+            }
+        }
+
+        const allDependencies = {
+            ...packageJson.dependencies,
+            ...packageJson.devDependencies,
+            ...packageJson.peerDependencies,
+        }
+
+        return {
+            status: SUCCESS,
+            body: JSON.stringify(allDependencies),
         }
     }
 }
--- test/version-reader.test.ts
+++ test/version-reader.test.ts
@@ -6,7 +6,20 @@ describe('Version Reader', async () => {
     const packageJsonFilePath = './test/package-test.json'
 
     beforeEach(async () => {
-        await fs.writeFile(packageJsonFilePath, JSON.stringify({dependencies: {'library-name': '1.0.0'}}, null, 4), 'utf8')
+        await fs.writeFile(packageJsonFilePath, JSON.stringify({
+            dependencies: {
+                'library-name': '1.0.0',
+                'library-name-2': '2.0.0',
+            },
+            devDependencies: {
+                '@types/library-name': '1.0.0',
+                '@types/library-name-2': '2.0.0',
+            },
+            peerDependencies: {
+                'library-name-peer': '1.0.0',
+                'library-name-peer-2': '2.0.0',
+            },
+        }, null, 4), 'utf8')
     })
 
     afterEach(async () => {
@@ -26,5 +39,64 @@ describe('Version Reader', async () => {
 
         expect(res.status).to.equal('ERROR')
         expect(res.body).to.equal('Library library-name-invalid not found')
+    })
+
+    it('should return all production dependencies', async () => {
+        const vReader = new VersionReader(packageJsonFilePath)
+        const res = await vReader.readMany('prod')
+
+        expect(res.status).to.equal('SUCCESS')
+        expect(res.body).to.equal('{"library-name":"1.0.0","library-name-2":"2.0.0"}')
+    })
+
+    it('should return an error if no production dependencies are found', async () => {
+        await fs.writeFile(packageJsonFilePath, JSON.stringify({}), 'utf8')
+        const vReader = new VersionReader(packageJsonFilePath)
+        const res = await vReader.readMany('prod')
+
+        expect(res.status).to.equal('ERROR')
+        expect(res.body).to.equal('No production dependencies found')
+    })
+
+    it('should return all development dependencies', async () => {
+        const vReader = new VersionReader(packageJsonFilePath)
+        const res = await vReader.readMany('dev')
+
+        expect(res.status).to.equal('SUCCESS')
+        expect(res.body).to.equal('{"@types/library-name":"1.0.0","@types/library-name-2":"2.0.0"}')
+    })
+
+    it('should return an error if no development dependencies are found', async () => {
+        await fs.writeFile(packageJsonFilePath, JSON.stringify({}), 'utf8')
+        const vReader = new VersionReader(packageJsonFilePath)
+        const res = await vReader.readMany('dev')
+
+        expect(res.status).to.equal('ERROR')
+        expect(res.body).to.equal('No development dependencies found')
+    })
+
+    it('should return all peer dependencies', async () => {
+        const vReader = new VersionReader(packageJsonFilePath)
+        const res = await vReader.readMany('peer')
+
+        expect(res.status).to.equal('SUCCESS')
+        expect(res.body).to.equal('{"library-name-peer":"1.0.0","library-name-peer-2":"2.0.0"}')
+    })
+
+    it('should return an error if no peer dependencies are found', async () => {
+        await fs.writeFile(packageJsonFilePath, JSON.stringify({}), 'utf8')
+        const vReader = new VersionReader(packageJsonFilePath)
+        const res = await vReader.readMany('peer')
+
+        expect(res.status).to.equal('ERROR')
+        expect(res.body).to.equal('No peer dependencies found')
+    })
+
+    it('should return all dependencies if no mode is given', async () => {
+        const vReader = new VersionReader(packageJsonFilePath)
+        const res = await vReader.readMany()
+
+        expect(res.status).to.equal('SUCCESS')
+        expect(res.body).to.equal('{"library-name":"1.0.0","library-name-2":"2.0.0","@types/library-name":"1.0.0","@types/library-name-2":"2.0.0","library-name-peer":"1.0.0","library-name-peer-2":"2.0.0"}')
     })
 })
